2dfd26e2852184c431a834735ed692232c1e266c
viszefaj <viszefaj8@gmail.com>
2022-07-07 11:09:37 +0000

    getting questions and answers from db

--- app-survey/src/pages/Page.js
+++ app-survey/src/pages/Page.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Question from "../Questions/Question";
 
-function Page() {
+import axios from "axios";
+
+export const Page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [question, setQuestion] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  const [answersLoading, setAnswersLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://8080-bmerkuri-finalproject-i52z2dfqen5.ws-eu51.gitpod.io/api/rest/question"
+      )
+      .then((res) => {
+        setQuestion(res.data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://8080-bmerkuri-finalproject-i52z2dfqen5.ws-eu51.gitpod.io/api/rest/answer"
+      )
+      .then((res) => {
+        setAnswers(res.data);
+        setAnswersLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Is Loading...</h2>;
+  }
+
   return (
-    <div>
-      <Question answers = {["hjdghs", "hello", "beni", "ale"]} question = "What is your name?"/>
-      <Question answers = {["20", "21", "24", "35"]} question = "How old are you?"/>
-    
-    </div>
+    <Question
+      key={question.questions[0].id}
+      question={question.questions[0].data.question}
+      answers={[
+        answers.answers[0].data.answer,
+        answers.answers[1].data.answer,
+        answers.answers[2].data.answer
+      ]}
+    />
   );
-}
+};
 
 export default Page;
