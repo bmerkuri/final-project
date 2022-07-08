cfc429b81c24619d4b734c28c3b15817fac853c5
Frajdi <frajdimalaj@gmail.com>
2022-07-08 15:14:22 +0000

    fixing fetching data and qusteion component

--- app-survey/src/Questions/Question.js
+++ app-survey/src/Questions/Question.js
@@ -13,19 +13,21 @@ export default function Question(props) {
     <Box sx={{display: "flex", justifyContent: "center", boxShadow:" rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px", margin: "3rem auto", backgroundColor: "#EEEEFF", padding: "2rem", width : "50%"}}>
       <FormControl>
         <FormLabel id="demo-radio-buttons-group-label">
-          {props.question}
+          {props?.question}
         </FormLabel>
+        {console.log("answerProps",props?.answers)}
         <RadioGroup
           aria-labelledby="demo-radio-buttons-group-label"
           defaultValue="female"
           name="radio-buttons-group"
         >
-          <FormControlLabel
+          {/* {props?.answers?.data.map(item => console.log("item",item))} */}
+          {/* <FormControlLabel
             value={props.answers[0]}
             control={<Radio />}
             label={props.answers[0]}
-          />
-          <FormControlLabel
+          /> */}
+          {/* <FormControlLabel
             value={props.answers[1]}
             control={<Radio />}
             label={props.answers[1]}
@@ -34,7 +36,7 @@ export default function Question(props) {
             value={props.answers[2]}
             control={<Radio />}
             label={props.answers[2]}
-          />
+          /> */}
         </RadioGroup>
       </FormControl>
     </Box>
--- app-survey/src/pages/Page.js
+++ app-survey/src/pages/Page.js
@@ -6,13 +6,15 @@ import axios from "axios";
 export const Page = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [question, setQuestion] = useState([]);
-  const [answers, setAnswers] = useState([]);
+  const [answer, setAnswers] = useState([]);
   const [answersLoading, setAnswersLoading] = useState(true);
+
+  var content = [];
 
   useEffect(() => {
     axios
       .get(
-        "https://8080-bmerkuri-finalproject-xomtuvqx7v4.ws-eu51.gitpod.io/api/rest/question"
+        "https://8080-bmerkuri-finalproject-44jjfxq8wsx.ws-eu51.gitpod.io/api/rest/question"
       )
       .then((res) => {
         setQuestion(res.data);
@@ -23,29 +25,70 @@ export const Page = () => {
   useEffect(() => {
     axios
       .get(
-        "https://8080-bmerkuri-finalproject-xomtuvqx7v4.ws-eu51.gitpod.io/api/rest/answer"
+        "https://8080-bmerkuri-finalproject-44jjfxq8wsx.ws-eu51.gitpod.io/api/rest/answer"
       )
       .then((res) => {
+        console.log("responseAnswer", res.data);
         setAnswers(res.data);
         setAnswersLoading(false);
       });
   }, []);
 
-  if (isLoading) {
+  if (isLoading && answersLoading) {
     return <h2>Is Loading...</h2>;
   }
 
   return (
-    <Question
-      key={question.questions[0].data.id}
-      question={question.questions[0].data.question}
-      answers={[
-        answers.answers[0].data.opsion1,
-        answers.answers[0].data.opsion2,
-        answers.answers[0].data.opsion3
-      ]}
-    />
+    <div>
+      {/* {console.log("1",answer?.answers.find(ele => ele.id === "1"))} */}
+      {console.log(answer?.answers?.find(element => element.id === 1))}
+      {console.log("answer", answer)}
+      {question?.questions.map((element) => {
+        return (
+          <Question
+          
+            key={element?.id}
+            question={element?.data?.question}
+            answers = {(answer?.answers?.find(ele => ele?.id === element?.id))}
+            // answers={[
+            //   answer.answers[0].data.opsion1,
+            //   answer.answers[0].data.opsion2,
+            //   answer.answers[0].data.opsion3
+            // ]}
+          />
+        );
+      })}
+    </div>
+
+    // <Question
+    //   key={question.questions[0].id}
+    //   question={question.questions[0].data.question}
+    //   answers={[
+    //     answer.answers[0].data.opsion1,
+    //     answer.answers[0].data.opsion2,
+    //     answer.answers[0].data.opsion3
+    //   ]}
+    // />
   );
+
+  // for (let i = 0; i < question.questions.length; i++) {
+
+  //   content.push(
+  //     <Question
+  //       key={question.questions[i].id}
+  //       question={question.questions[i].data.question}
+  //       answers={[
+  //         answer.answers[i].data.opsion1,
+  //         answer.answers[i].data.opsion2,
+  //         answer.answers[i].data.opsion3
+  //       ]}
+  //     />
+  //   );
+  // }
+
+  // content.map((element) => {
+  //   return element;
+  // });
 };
 
 export default Page;
