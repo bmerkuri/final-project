4886c9265504a85d4ee33b3cf29f9ea78708cdf1
viszefaj <viszefaj8@gmail.com>
2022-07-08 09:00:10 +0000

    Adding different values in db

--- hasura-migrations/seeds/default/dummy.sql
+++ hasura-migrations/seeds/default/dummy.sql
@@ -59,3 +59,64 @@ DO UPDATE SET
  "updated_at" = EXCLUDED."updated_at",
 "opens_at" = EXCLUDED."opens_at",
 "closes_at" = EXCLUDED."closes_at";
+
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(1,1,'{"question":"What is level of knowledge of CSS"}','question1');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(2,2,'{"question":"Which programming language is the best for web development?"}','question2');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(3,3,'{"question":"Which back end framework is the best for web development?"}','question3');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(4,4,'{"question":"Most powerful programming language?"}','question4');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(5,5,'{"question":"Most used programming language?"}','question5');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(6,6,'{"question":"Javascript is a :"}','question6');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(7,7,'{"question":"Html is a :"}','question7');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(8,8,'{"question":"On Rails"}','question8');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(9,9,'{"question":"Dart is a langauge developed by"}','question9');
+
+INSERT INTO "public"."questions" ("id","board_id","data","type")
+VALUES(10,10,'{"question":"Php is used for :"}','question10');
+
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(1,1,1,1,10,'{"opsion1":"30%","question2":"60%","opsion3":"90%"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(2,2,2,2,10,'{"opsion1":"Javascript","question2":"Php","opsion3":"Ruby"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(3,3,3,3,10,'{"opsion1":"NodeJs","question2":"Laravel","opsion3":"Django"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(4,4,4,4,10,'{"opsion1":"Python","question2":"Java","opsion3":"Javascript"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(5,5,5,5,10,'{"opsion1":"Java","question2":"Ruby","opsion3":"C"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(6,6,6,6,10,'{"opsion1":"Programming Language","question2":"Framework","opsion3":"Tool"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(7,7,7,7,10,'{"opsion1":"Programming Language","question2":"Hypertext Markup Language","opsion3":"Framework"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(8,8,8,8,10,'{"opsion1":"Google","question2":"Microsoft","opsion3":"By me"}');
+
+INSERT INTO "public"."answers"("id","board_id","user_id","question_id","score","data")
+VALUES(9,9,9,9,10,'{"opsion1":"Web development","question2":"Mobile development","opsion3":"Desktop Development"}');
+
+
